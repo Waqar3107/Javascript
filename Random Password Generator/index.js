f4f0af3cd2f00901e506682cff8301a8c7d44390
Waqar3107 <sajeelahmed33.sa@gmail.com>
2026-01-20 12:10:32 +0500

    Random Password generator

---
+++ Random Password Generator/index.js
@@ -0,0 +1,38 @@
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+    let allChars = "";
+    if (includeLowercase) allChars += lowercaseChars;
+    if (includeUppercase) allChars += uppercaseChars;
+    if (includeNumbers) allChars += numberChars;
+    if (includeSymbols) allChars += symbolChars;
+
+    if (allChars.length === 0) return "Please select at least one character type.";
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+
+    return password;
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(
+    passwordLength,
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+);
+
+console.log(`Generated password: ${password}`);
